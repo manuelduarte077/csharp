2132daa409adc79d690445ae0859ef2b2ca1fff3
manuelduarte077 <carlosd99m@gmail.com>
2026-02-04 23:57:48 -0600

    add model user

--- MovieApi/Data/ApplicationDbContext.cs
+++ MovieApi/Data/ApplicationDbContext.cs
@@ -12,4 +12,5 @@ public class ApplicationDbContext : DbContext
     // Agregar las entidades (Models)
     public DbSet<Category> Categories { get; set; }
     public DbSet<Movie> Movies { get; set; }
+    public DbSet<User> Users { get; set; }
 }
---
+++ MovieApi/Models/User.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.Models;
+
+public class User
+{
+    [Key] 
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+}
